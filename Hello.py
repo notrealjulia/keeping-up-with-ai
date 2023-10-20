c16346de6b26bcf4320105b279b30352beed20c2
Maybe real Julia <julia.spicina@gmail.com>
2023-10-20 11:22:32 +0000

    modified:   Hello.py 	modified:   requirements.txt

--- Hello.py
+++ Hello.py
@@ -1,51 +1,24 @@
-# Copyright (c) Streamlit Inc. (2018-2022) Snowflake Inc. (2022)
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-
 import streamlit as st
-from streamlit.logger import get_logger
+from transformers import pipeline
 
-LOGGER = get_logger(__name__)
-
+# Create a text summarization pipeline
+summarizer = pipeline("summarization", model="t5-small")
 
 def run():
     st.set_page_config(
-        page_title="Hello",
-        page_icon="👋",
+        page_title="Text Summarization",
+        page_icon="📝",
     )
 
-    st.write("# Welcome to Streamlit! 👋")
+    st.write("# Welcome to the Text Summarization App! 📝")
 
-    st.sidebar.success("Select a demo above.")
+    # Create a text input in Streamlit
+    text = st.text_input('Enter your text')
 
-    st.markdown(
-        """
-        Streamlit is an open-source app framework built specifically for
-        Machine Learning and Data Science projects.
-        **👈 Select a demo from the sidebar** to see some examples
-        of what Streamlit can do!
-        ### Want to learn more?
-        - Check out [streamlit.io](https://streamlit.io)
-        - Jump into our [documentation](https://docs.streamlit.io)
-        - Ask a question in our [community
-          forums](https://discuss.streamlit.io)
-        ### See more complex demos
-        - Use a neural net to [analyze the Udacity Self-driving Car Image
-          Dataset](https://github.com/streamlit/demo-self-driving)
-        - Explore a [New York City rideshare dataset](https://github.com/streamlit/demo-uber-nyc-pickups)
-    """
-    )
-
+    # When the 'Summarize' button is pressed, summarize the input text
+    if st.button('Summarize'):
+        summary = summarizer(text, max_length=100, min_length=30, do_sample=False)
+        st.write(summary[0]['summary_text'])
 
 if __name__ == "__main__":
     run()
